fec008cbf12a3ecaf4f66d082bd78395e7ca861e
Mushfiq <mushfiq2808@gmail.com>
2025-06-09 12:52:59 +0600

    fixed more issues v5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'react-hot-toast';
 import Navbar from '@/components/Navbar'; // Import the Navbar
 
-const inter = Inter({ subsets: ['latin'] });
+
 
 export const metadata: Metadata = {
   title: 'Commerze',
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body >
           <Navbar /> {/* Include the Navbar here */}
           <main className="container mx-auto p-4">
             {children}
